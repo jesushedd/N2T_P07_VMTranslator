b5ee95d68579d35a74ff0a18b3b6c713f737cc88
jesushedd <jesushe99@gmail.com>
2024-08-29 15:46:11 -0500

    writeArithmetic method, progress

--- src/main/java/CodeWriter.java
+++ src/main/java/CodeWriter.java
@@ -1,27 +1,43 @@
+import org.apache.commons.io.function.IOSpliterator;
+
 import javax.imageio.IIOException;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CodeWriter {
 
     private final BufferedWriter outWriter;
 
-    private final Map<String, String> operate2Values = new HashMap<>();
+    private final Set<String> twoValuesCommands = new HashSet<>();
+
+    private final Set<String> oneValueCommands = new HashSet<>();
 
     public CodeWriter(Path outFile) throws IOException {
         //open file ready to write
         outWriter = Files.newBufferedWriter(outFile);
-        //fill mappings
-        operate2Values.put()
+        //fill commands sets
+        twoValuesCommands.addAll(List.of("add", "sub", "eq", "gt", "lt", "and", "or"));
+        oneValueCommands.addAll(List.of("neg", "not"));
     }
 
-    public void writeArithmetic(String arithmeticCommand) throws IIOException{
+    public void writeArithmetic(String arithmeticCommand) throws IOException{
+        // TODO write comment
+        outWriter.write(arithmeticCommand);
+        outWriter.flush();
+        notify();close();
+        //
+        if (oneValueCommands.contains(arithmeticCommand)){
+            //TODO
+        } else if (twoValuesCommands.contains(arithmeticCommand)) {
+            //TODO
+
+        } else {
+            throw new IllegalArgumentException();
+        }
+
         //command sp[-2] =  sp[-2] sp[-1]
         return;
     }
