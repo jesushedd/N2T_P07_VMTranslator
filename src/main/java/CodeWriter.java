3e79250be1861e9944c94057e862251e8b0bd167
jesushedd <jesushe99@gmail.com>
2024-08-31 14:24:07 -0500

    add translating comparison arithmetic

--- src/main/java/CodeWriter.java
+++ src/main/java/CodeWriter.java
@@ -2,6 +2,7 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.PipedReader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,6 +26,8 @@ public class CodeWriter {
 
     private final String className;
 
+    private int relationalCounter;
+
 
 
     public CodeWriter(Path outFile) throws IOException {
@@ -32,6 +35,7 @@ public class CodeWriter {
         outWriter = Files.newBufferedWriter(outFile);
         //save class name
         className = FilenameUtils.getBaseName(outFile.toString());
+        relationalCounter = 0;
         fillMaps();
 
     }
@@ -41,6 +45,7 @@ public class CodeWriter {
     CodeWriter(Writer writer, String nameOfClass) {
         outWriter = new BufferedWriter(writer);
         className = nameOfClass;
+        relationalCounter = 0;
         fillMaps();
     }
 
@@ -103,7 +108,7 @@ public class CodeWriter {
                             "M=M" + arithmeticLogicCommands.get(arithmeticCommand) + "D\r\n"; // save result of operation
         } else if (relationalCommands.containsKey(arithmeticCommand)) {
             asmInstructions = //get top value (y)
-                    "@SP\r\n" +  //select stack pointer
+                            "@SP\r\n" +  //select stack pointer
                             "AM=M-1\r\n" + //dereference it and subtract 1, select top stack value ,update stack pointer by -1
                             "D=M\r\n" + //save top stack value
                             //get x
@@ -112,22 +117,23 @@ public class CodeWriter {
                             //save comparison
                             "D=M-D\r\n" +
                             // if condition given is true; jump to save true
-                            "@SAVE_TRUE\r\n" +
+                            "@SAVE_TRUE" + relationalCounter +"\r\n" +
                             "D;" + relationalCommands.get(arithmeticCommand) + "\r\n" +
                             //if not save false on new stack top value
                             "@SP\r\n" +
                             "A=M-1\r\n" +
                             "M=0\r\n" +
 
-                            "@CONTINUE\r\n" +
+                            "@CONTINUE"+ relationalCounter +"\r\n" +
                             "0;JMP\r\n" +
 
-                            "(SAVE_TRUE)\r\n" +
+                            "(SAVE_TRUE" +  relationalCounter +")\r\n" +
                             "@SP\r\n" +
                             "A=M-1\r\n" +
-                            "M=1\r\n" +
+                            "M=-1\r\n" +
 
-                            "(CONTINUE)\r\n";
+                            "(CONTINUE" + relationalCounter + ")\r\n";
+            relationalCounter++;
         } else throw new IllegalArgumentException();
         return asmInstructions;
 
@@ -150,6 +156,9 @@ public class CodeWriter {
             asmInstructions = assemblePointer(pushOrPopCommand, index);
         } else throw new IllegalArgumentException();
 
+        //write asm instructions
+        outWriter.write(asmInstructions);
+
 
     }
 
@@ -163,10 +172,10 @@ public class CodeWriter {
                                 "AM=M-1\r\n" +
                                 "D=M\r\n" +
                                 thisOrThat +
-                                "M=D";
+                                "M=D\r\n";
         } else if (pushOrPopCommand.equals("push")) {
             asmInstructions =   thisOrThat +
-                                "D=M" +
+                                "D=M\r\n" +
 
                                 "@SP\r\n" +
                                 "A=M\r\n" +
