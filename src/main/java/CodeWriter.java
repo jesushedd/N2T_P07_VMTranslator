26769977ca2aed4539f028a3da763e9cdd1f9dd6
jesushedd <jesushe99@gmail.com>
2024-08-30 19:33:38 -0500

    assembleArithmetic isntrucction implementation

--- src/main/java/CodeWriter.java
+++ src/main/java/CodeWriter.java
@@ -10,20 +10,28 @@ public class CodeWriter {
 
     private final BufferedWriter outWriter;
 
-    private final Map<String, String> twoValueCommands = new HashMap<>();
+    /*AArithmetic and Logic Commands that operate 2 values*/
+    private final Map<String, String> arithmeticLogicCommands = new HashMap<>();
 
+    /*AArithmetic and Logic Commands that operate 1 value*/
     private final Map<String, String> oneValueCommands = new HashMap<>();
 
-    private void fillMaps(){
-        //fill commands mapping
-        twoValueCommands.put("add", "+");
-        twoValueCommands.put("sub", "-");
-        twoValueCommands.put("eq", "==");
-        twoValueCommands.put("gt", ">");
-        twoValueCommands.put("lt", "<");
-        twoValueCommands.put("and", "&");
-        twoValueCommands.put("or", "|");
+    /*Relational Commands that operate 2 values*/
+    private final Map<String, String> relationalCommands = new HashMap<>();
 
+    private void fillMaps(){
+        //fill arithmetic and logic mapping
+        arithmeticLogicCommands.put("add", "+");
+        arithmeticLogicCommands.put("sub", "-");
+        arithmeticLogicCommands.put("and", "&");
+        arithmeticLogicCommands.put("or", "|");
+
+        //fill relational mapping
+        relationalCommands.put("eq", "JEQ");
+        relationalCommands.put("gt", "JGT");
+        relationalCommands.put("lt", "JLT");
+
+        //fil one value mapping
         oneValueCommands.put("neg", "-");
         oneValueCommands.put("not","!");
 
@@ -56,17 +64,49 @@ public class CodeWriter {
 
         String asmInstructions = null;
         if (oneValueCommands.containsKey(arithmeticCommand)){ //for commands that require only the most top value on Stack
-            //TODO
             asmInstructions = "@SP\r\n" + //select stack pointer
                               "A=M-1\r\n" + // dereference it and subtract 1 , select top stack top value
                               "M=" + oneValueCommands.get(arithmeticCommand) + "M\r\n";  // update top value with new value commanded
-        } else if (twoValueCommands.containsKey(arithmeticCommand)) { //for commands that require the 2 most top values on Stack
-            //TODO
+        } else if (arithmeticLogicCommands.containsKey(arithmeticCommand)) { //for commands that require the 2 most top values on Stack
 
-        } else {
-            throw new IllegalArgumentException();
-        }
-        //command sp[-2] =  sp[-2] sp[-1]
+            asmInstructions = //get top value (y)
+                              "@SP\r\n" +  //select stack pointer
+                              "AM=M-1\r\n" + //dereference it and subtract 1, select top stack value ,update stack pointer by -1
+                              "D=M\r\n" + //save top stack value
+
+                              "@SP\r\n" + // select new stack pointer (top value)
+                              "A=M-1\r\n" + // dereference it and subtract 1,  this is the second to top value from stack (x), select it
+
+                              // execute operation and save it
+                              "M=M" + arithmeticLogicCommands.get(arithmeticCommand) + "D\r\n"; // save result of operation
+        } else if (relationalCommands.containsKey(arithmeticCommand)){
+            asmInstructions = //get top value (y)
+                                "@SP\r\n" +  //select stack pointer
+                                "AM=M-1\r\n" + //dereference it and subtract 1, select top stack value ,update stack pointer by -1
+                                "D=M\r\n" + //save top stack value
+                              //get x
+                                "@SP\r\n" +
+                                "A=M-1\r\n" +
+                              //save comparison
+                                "D=M-D\r\n" +
+                              // if condition given is true; jump to save true
+                                "@SAVE_TRUE\r\n" +
+                                "D;" + relationalCommands.get(arithmeticCommand) + "\r\n" +
+                              //if not save false on new stack top value
+                                "@SP\r\n" +
+                                "A=M-1\r\n" +
+                                "M=0\r\n" +
+
+                                "@CONTINUE\r\n" +
+                                "0;JMP\r\n" +
+
+                                "(SAVE_TRUE)\r\n" +
+                                "@SP\r\n" +
+                                "A=M-1\r\n" +
+                                "M=1\r\n" +
+
+                                "(CONTINUE)\r\n";
+        } else throw new IllegalArgumentException();
         return asmInstructions;
 
     }
--- src/main/java/TestWriter.java
+++ src/main/java/TestWriter.java
@@ -10,11 +10,32 @@ public class TestWriter {
     @Test
     public void testWriteArithmetic() throws IOException {
         StringWriter out = new StringWriter();
+        // neg not
         codeWriter = new CodeWriter(out);
         codeWriter.writeArithmetic("neg");
         codeWriter.writeArithmetic("not");
         codeWriter.close();
-        assertEquals("// neg\r\n@SP\r\nA=M-1\r\nM=-M\r\n// not\r\n@SP\r\nA=M-1\r\nM=!M\r\n", out.toString());
+        assertEquals(  "// neg\r\n" +
+                                "@SP\r\n" +
+                                "A=M-1\r\n" +
+                                "M=-M\r\n" +
+                                "// not\r\n" +
+                                "@SP\r\n" +
+                                "A=M-1\r\n" +
+                                "M=!M\r\n",
+                                out.toString());
+
+        // + , - , & , |
+        out = new StringWriter();
+        codeWriter = new CodeWriter(out);
+        codeWriter.writeArithmetic("add");
+        codeWriter.writeArithmetic("sub");
+        codeWriter.writeArithmetic("and");
+        codeWriter.writeArithmetic("or");
+        codeWriter.close();
+        assertEquals("push constant 10\n" +
+                "pop local 0\n" +
+                "push constant 21", out.toString());
 
 
 
